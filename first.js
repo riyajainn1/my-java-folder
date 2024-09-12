5bd01a8bc076916bc2e784a1f770304c9606f0bf
riyajainn1 <riyajain8856@gmail.com>
2024-09-12 11:14:48 +0530

    js

---
+++ first.js
@@ -0,0 +1,50 @@
+// let a=true
+// let b=false
+// let score=80
+// if(score>75){
+//     console.log("GRADE A")
+// }
+// else if(score>65 && score<70){
+//     console.log("GRADE B")
+// }
+// else if(score>40 && score<65){
+//     console.log("GRADE C")
+// }
+// else(score>10 && score<30){
+//     console.log("GRADE E")
+// }
+// let arr=[ 'riyaa',4,true,56]
+// console.log(arr[0])
+// let std={
+//     id:1,
+//     name:'riya',
+//     age:19,
+//     isPass:true
+
+// }
+// std.name="jain"
+// console.log(std.name)
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(344565,928365353)
+// const sum=(a,b)=>{
+//     console.log(a+b);
+// }
+// sum(45,54)
+// 
+// function sum(a,b){
+//     console.log("helloo")
+//     return a+b
+//     console.log('hiiii')
+// }
+// console.log(sum(5+4));
+let arr=[1,2,3,4,5,6,7,8]
+for (let i of arr){
+    console.log(i)
+}
+
+for (let i in arr){
+    console.log(i)
+}
+
