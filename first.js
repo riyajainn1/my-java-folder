bd70eeb6d04523d88b6a217b51f29fad37499a50
riyajainn1 <riyajain8856@gmail.com>
2024-09-17 14:25:37 +0530

    insta

--- first.js
+++ first.js
@@ -47,13 +47,143 @@
 // for (let i in arr){
 //     console.log(i)
 // }
-function sum(a){
-    console.log(` ${a.name}     ${a.lastName}`);
-}
-let obj={
-    id:2,
-    name:"riya",
-    lastName:"jain"
-}
-sum(obj)
+// function sum(a){
+//     console.log(` ${a.name}     ${a.lastName}`);
+// }
+// let obj={
+//     id:2,
+//     name:"riya",
+//     lastName:"jain"
+// }
+// sum(obj)
 
+// [ 
+// // console.log(a)
+// // let a=5
+
+
+
+// // let a
+// // console.log(a)
+// // a=5
+// ]
+
+
+
+// var a=5
+// {
+//     var a=10
+//     console.log(a)
+// }
+// console.log(a)
+  
+
+
+
+// let a=5
+// {
+//     let a=10
+//     console.log(a)
+// }
+// console.log(a)
+
+
+// function outer (a){
+//     console.log(a);
+//     a()
+// }
+// outer(function inner(){
+//     console.log('hello');
+// }
+
+// function outer(){
+//     console.log('outer');
+//     function innner(){
+//         console.log('inner')
+//     }
+//     return inner
+// }
+// let inner=outer()
+// inner()
+
+
+
+
+
+// arr me objects alag krna
+
+// let arr=[1,2,3,'riya',false]
+// function number(arr){
+//     let result=[]
+//     for(  let i of arr){ 
+//         if( typeof i==='number'){
+//             result.push(i)
+//         }
+// }
+// return result
+// }
+
+// console.log(number(arr));
+
+
+
+
+
+
+
+
+
+
+// 
+
+
+
+
+
+
+
+// 
+
+
+
+
+function step1(fn){
+    setTimeout(()=>{
+        console.log('select photo')
+fn()
+    },4000)
+   
+
+}
+function step2(fn){
+    setTimeout(()=>{
+        console.log('filter')
+fn()
+    },2000)
+   
+
+}
+function step3(fn){
+    setTimeout(()=>{
+        console.log('caption')
+fn()
+    },3000)
+   
+
+}
+function step4(){
+    setTimeout(()=>{
+        console.log('post')
+        
+
+    },1000)
+   
+
+}
+step1(function(){
+    step2(function(){
+        step3(function(){
+            step4()
+        })
+    })
+})
